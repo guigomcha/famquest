35d292d023a4e96b13ab5d58c2fde49479d9b46a
A809921 <guillermo.gomezchavez@atos.net>
2024-11-01 11:46:21 +0100

    canvas refactored into leaflet class

--- components/front-manager/MapContainer.js
+++ components/front-manager/MapContainer.js
@@ -3,6 +3,7 @@ import 'leaflet-defaulticon-compatibility';
 import './node_modules/leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
+import {CanvasLayer} from "./CanvasLeaflet"
 import React, { useEffect, useRef, useState } from "react";
 
 const mapStyles = {
@@ -10,6 +11,7 @@ const mapStyles = {
     width: "100%",
   };
   
+const scale = 13;
 const defaultCenter = {
   lat: 37.31942002016036,
   lng: -6.0678988062297465,
@@ -22,80 +24,19 @@ const MapContainer = ( {locations, spots } ) => {
   const locs = useRef(null);
 
   const prepareMap = () => {
-    hideMap();
     console.log("locations in prepare: "+ JSON.stringify(locs))
     // After the map is loaded, reveal the area around each marker
     if (mapRef.current && locs.current) {
       locs.current.forEach((location) => {
         console.log("used coordinate: "+ location.id)
-        revealMapAroundMarker(location);
+        canvasRef.current.revealArea(
+          mapRef.current.latLngToContainerPoint(L.latLng(location.latitude, location.longitude)),
+          scale
+        )
       });
     }
   };
 
-  const calculateRadius = (scale) => {
-    // Cap radius based on the scale thresholds
-    const minRadius = 50;
-    const maxRadius = 200;
-    const minScale = 500;
-    const maxScale = 200000;
-  
-    // If scale is greater than the max scale, cap radius to maxRadius
-    if (scale >= maxScale) return maxRadius;
-  
-    // If scale is less than the min scale, cap radius to minRadius
-    if (scale <= minScale) return minRadius;
-  
-    // Linear interpolation to smoothly adjust radius based on scale
-    const ratio = (scale - minScale) / (maxScale - minScale);
-    const radius = minRadius + ratio * (maxRadius - minRadius);
-  
-    return radius;
-  };
-  const revealMapAroundMarker = (location) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-  
-    // Assuming you have a reference to your leaflet map using ref (mapRef)
-    const map = mapRef.current;
-  
-    if (!map) return;
-  
-    // Leaflet method to convert LatLng to pixel position on the map
-    const latLng = L.latLng(location.latitude, location.longitude);
-    const point = map.latLngToLayerPoint(latLng);  // Converts LatLng to a pixel position
-  
-    // Convert the center of the map to a pixel point
-    const centerLatLng = map.getCenter();
-    const centerPoint = map.latLngToLayerPoint(centerLatLng);
-  
-    // Calculate offsets based on zoom level
-    const scale = map.getZoom();  // Leaflet zoom level
-    const offsetX = point.x - centerPoint.x + canvas.width / 2;
-    const offsetY = point.y - centerPoint.y + canvas.height / 2;
-  
-    // Clear a circular area on the canvas to reveal the map beneath the marker
-    ctx.globalCompositeOperation = "destination-out";  // Set to reveal the area
-    ctx.beginPath();
-    ctx.arc(offsetX, offsetY, calculateRadius(scale), 0, Math.PI * 2, true);
-    ctx.fill();
-  };
-  const hideMap = () => {
-    const canvas = canvasRef.current;
-    if (!canvas) {
-      return
-    }
-    const ctx = canvas.getContext("2d");
-
-    // Set canvas size based on the map container
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-
-    // Fill the entire canvas with a black mask initially
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  };
 
   // 
   useEffect(() => {
@@ -106,19 +47,52 @@ const MapContainer = ( {locations, spots } ) => {
   useEffect(() => {
     if (!mapRef.current) {
       console.log("Adding event")
-      mapRef.current = L.map('mapId').setView([defaultCenter.lat, defaultCenter.lng], 13);
-      mapRef.current.addEventListener('zoom', prepareMap);
-      mapRef.current.addEventListener('zoomend', prepareMap);
-      mapRef.current.addEventListener('move', prepareMap);
-      mapRef.current.addEventListener('moveend', prepareMap);
-      mapRef.current.addEventListener('drag', prepareMap);
-      mapRef.current.addEventListener('idle', prepareMap);
-      mapRef.current.addEventListener('dragend', prepareMap);
-      mapRef.current.addEventListener('resize', prepareMap);
-      
+      mapRef.current = L.map('mapId').setView([defaultCenter.lat, defaultCenter.lng], scale);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors',
       }).addTo(mapRef.current);
+          // Create layer groups
+
+      // Add layer group with circles
+      const layerGroup = L.layerGroup().addTo(mapRef.current);
+      L.circle([defaultCenter.lat+0.05, defaultCenter.lng+0.05], { color: 'blue', fillColor: 'blue', fillOpacity: 0.5, radius: 200 }).addTo(layerGroup);
+      L.circle([defaultCenter.lat+0.08, defaultCenter.lng+0.05], { color: 'red', fillColor: 'red', fillOpacity: 0.5, radius: 100, stroke: false }).addTo(layerGroup);
+      L.circle([defaultCenter.lat+0.06, defaultCenter.lng+0.05], { color: 'green', fillColor: 'green', fillOpacity: 0.5, radius: 100 }).addTo(layerGroup);
+
+      // Add feature group with a popup and shapes
+      const featureGroup = L.featureGroup().addTo(mapRef.current);
+      // L.popup().setContent('Popup in FeatureGroup').setLatLng([defaultCenter.lat+0.15, defaultCenter.lng+0.05]).openOn(mapRef.current);
+      L.circle([defaultCenter.lat+0.05, defaultCenter.lng+0.01], { color: 'purple', radius: 200 }).addTo(featureGroup);
+      L.rectangle([[defaultCenter.lat+0.15, defaultCenter.lng+0.19],[defaultCenter.lat+0.05, defaultCenter.lng+0.25]], { color: 'purple', weight: 1 }).addTo(featureGroup);
+      
+      // Create a blue canvas overlay
+      canvasRef.current = new CanvasLayer();
+      featureGroup.addLayer(canvasRef.current);
+      canvasRef.current.addTo(mapRef.current);
+
+      // Events
+      const handleEvent = () => {
+        if (canvasRef.current) {
+          canvasRef.current.redraw(mapRef.current); // Pass current map
+        }
+      };
+      mapRef.current.addEventListener('zoom', handleEvent);
+      mapRef.current.addEventListener('zoomend', handleEvent);
+      mapRef.current.addEventListener('move', handleEvent);
+      mapRef.current.addEventListener('moveend', handleEvent);
+      mapRef.current.addEventListener('drag', handleEvent);
+      mapRef.current.addEventListener('idle', handleEvent);
+      mapRef.current.addEventListener('dragend', handleEvent);
+      mapRef.current.addEventListener('resize', handleEvent);
+      
+      // Create overlay controls
+      const overlays = {
+        // "Marker with popup": marker,
+        "Layer group with circles": layerGroup,
+        "Feature group": featureGroup
+      };
+
+      L.control.layers(null, overlays, { collapsed: false }).addTo(mapRef.current);
     }
   
     return () => {
@@ -138,6 +112,7 @@ const MapContainer = ( {locations, spots } ) => {
             iconSize: [28, 75],
             iconAnchor: [22, 94],
             popupAnchor: [-3, -76],
+            zIndexOffset: 1
           }),
         })
           .addTo(mapRef.current)
@@ -149,7 +124,7 @@ const MapContainer = ( {locations, spots } ) => {
   return (
     <div style={{ position: "relative", width: "100%", height: "100%"}}>
       {/* Canvas for revealing part of the map */}
-      <canvas ref={canvasRef} style={{position: "absolute", width: "100%", height: "100%", zIndex: 10000, pointerEvents: "none"}} />
+      {/* <canvas ref={canvasRef} style={{position: "absolute", width: "100%", height: "100%", zIndex: 10000, pointerEvents: "none"}} /> */}
       <div id="mapId" style={{ height: '100vh', width: '100vw' }}></div>;
     </div>
   );
