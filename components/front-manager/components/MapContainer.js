11226d742563ea7b7ba827b22deb8dde2693d3b1
A809921 <guillermo.gomezchavez@atos.net>
2024-12-03 20:24:13 +0100

    fixing visible locations

--- components/front-manager/components/MapContainer.js
+++ components/front-manager/components/MapContainer.js
@@ -38,8 +38,8 @@ const MapContainer = ( {locations, spots, handleMenuChange } ) => {
     if (mapRef.current && locs.current && fogLayer.current) {
       locs.current.forEach((location) => {
         console.info("checking coordinate: "+ JSON.stringify(location))
-        if (location.refId == 0) {
-          console.info("Ignoring location from spot: ");
+        if (location.refId != 0) {
+          console.info("Ignoring location from spot: ", location);
           return;
         }
         //Uncover new area in fog
